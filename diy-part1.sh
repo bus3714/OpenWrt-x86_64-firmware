7c56d959edc8c75e35c17f7cefe8056ad737cd0d
bus3714 <czbhs1986@qq.com>
2024-02-03 14:19:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git wechatpush https://github.com/tty228/luci-app-wechatpush' >>feeds.conf.default
